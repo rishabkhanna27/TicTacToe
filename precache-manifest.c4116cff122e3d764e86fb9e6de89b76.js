872c980a5abc331d07bfa9b80c66e8c09cfcb596
rishabkhanna27 <rishabkhanna27@gmail.com>
2020-06-15 01:57:54 +0530

    Updates

---
+++ precache-manifest.c4116cff122e3d764e86fb9e6de89b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33c67e411ad757dff6d007835743ec7",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "0c39baf6e08f613fa397",
+    "url": "/TicTacToe/static/css/main.084abed0.chunk.css"
+  },
+  {
+    "revision": "1147046ee51a66f96b5a",
+    "url": "/TicTacToe/static/js/2.8b6d81ee.chunk.js"
+  },
+  {
+    "revision": "0c39baf6e08f613fa397",
+    "url": "/TicTacToe/static/js/main.16275ebd.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  }
+]);
